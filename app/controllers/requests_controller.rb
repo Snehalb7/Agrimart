68c99109fab29da8ecda9391dde18a39bb49c19e
Shriyog <shriyog1995@gmail.com>
2015-04-28 21:29:03 +0530

    request incomplete

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,21 @@
 class RequestsController < ApplicationController
+
+    def new
+        @request = Request.new
+    end
+    
+    def create
+        @request = Request.new(request_params)
+        if @request.save
+            redirect_to traders_path
+            flash.notice = "Request Created"
+        else
+            render "new"
+        end   
+    end
+    
+    def request_params
+        params.require(:request).permit(:title, :quantity ,:bid_price)
+    end
+    
 end
